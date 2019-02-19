7c058a3a4f7105b45b3eef7a9f2ed5353da5b7f5
ironmansastri <saisagar729@gmail.com>
2019-02-19 20:13:14 +0530

    Updated array.js and hashmap.js

---
+++ array.js
@@ -0,0 +1,39 @@
+
+
+class MyArray {
+
+    constructor(){
+        this.length = length;
+        this.data = {};
+    }
+
+    get(value){
+        return this.data[value];
+    }
+
+    push(value){
+        this.data[this.length] = value;
+        this.length++;
+        return this.data;
+    }
+
+    pop(){
+        var lastItem = this.data[this.length-1];
+        delete this.data[this.length-1];
+        this.length--;
+        return lastItem;
+    }
+
+    deleteAtIndex(index){
+        const item = this.data[index];
+        this.shiftItems(index);
+        return item;
+    }
+    shiftItems(index){
+        for (let i = index; i < this.length - 1; i++) {
+            this.data[i] = this.data[i + 1];
+          }
+          delete this.data[this.length - 1];
+          this.length--;
+    }
+}
---
+++ hashmaps.js
@@ -0,0 +1,51 @@
+
+
+class MyHashmap {
+
+    constructor(){
+        this.data = new Array();
+    }
+
+    _hash(key) {
+        let hash = 0;
+        for(let i = 0; i < key.length; i++){
+            hash = (hash + key.charCodeAt(i)*i);
+        }
+        return hash;
+    }
+
+    set(key, value){
+        let hashedAddress = this._hash(key);
+
+        if(!this.data[hashedAddress]) {
+            this.data[hashedAddress] = []
+        }
+
+        this.data[hashedAddress].push([key,value]);
+        return this.data;
+    }
+
+    get(key) {
+        let hashedAddress = this._hash(key);
+        let getValueFromAddress = this.data[hashedAddress];
+
+        if(getValueFromAddress){
+            for(let i = 0; i < getValueFromAddress.length; i++){
+                if(getValueFromAddress[i][0] === key){
+                    return getValueFromAddress[i][1];
+                }
+            }
+        }
+        return undefined;
+    }
+
+    keys(){
+        const keysArray = [];
+        for (let i = 0; i < this.data.length; i++){
+          if(this.data[i]){
+            keysArray.push(this.data[i][0][0])
+          }
+        }
+        return keysArray;
+      }
+}
